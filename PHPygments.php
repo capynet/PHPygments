769e6b53fb20f5ee5c982bfc31206907c6dc98a8
capy <capy.net@gmail.com>
2013-04-17 04:29:01 +0200

    apply format code.

--- PHPygments.php
+++ PHPygments.php
@@ -7,55 +7,53 @@
  * Author URI: http://ecapy.com/
  * License: GPL v3
  */
-class PHPygments
-{
+class PHPygments {
 
-    /**
-     * Sends code to highlight to pygments across using "bind.py" and returns the result
-     * @param $code
-     * @param $language
-     * @param string $style
-     * @param string $linenumbers
-     * @return array "code" the processed code "styles" a reference of css needed files.
-     */
-    static function render($code, $language, $style = "default", $linenumbers = "False")
-    {
+  /**
+   * Sends code to highlight to pygments across using "bind.py" and returns the result
+   * @param $code
+   * @param $language
+   * @param string $style
+   * @param string $linenumbers
+   * @return array "code" the processed code "styles" a reference of css needed files.
+   */
+  static function render($code, $language, $style = "default", $linenumbers = "False") {
 
-        $pygments_bind_app = "python " . dirname(__FILE__) . "/bind.py";
+    $pygments_bind_app = "python " . dirname(__FILE__) . "/bind.py";
 
-        // Create a temporary file as bridge for code...
-        $temp_name = tempnam("/tmp", "pygmentize_");
-        $file_handle = fopen($temp_name, "w");
-        fwrite($file_handle, $code);
-        fclose($file_handle);
-        chmod($temp_name, 0777);
+    // Create a temporary file as bridge for code...
+    $temp_name = tempnam("/tmp", "pygmentize_");
+    $file_handle = fopen($temp_name, "w");
+    fwrite($file_handle, $code);
+    fclose($file_handle);
+    chmod($temp_name, 0777);
 
-        //Settings
-        $pygments_bind_params = array(
-            "--sourcefile" => $temp_name,
-            "--style" => $style,
-            "--lang" => $language,
-            "--linenumbers" => $linenumbers
-        );
+    //Settings
+    $pygments_bind_params = array(
+      "--sourcefile" => $temp_name,
+      "--style" => $style,
+      "--lang" => $language,
+      "--linenumbers" => $linenumbers
+    );
 
-        $params = " ";
-        foreach ($pygments_bind_params as $k => $v) {
-            $params .= $k . "=" . $v . " ";
-        }
-
-        $command = $pygments_bind_app . " " . rtrim($params);
-        $output = array();
-        $retval = -1;
-
-        exec($command, $output, $retval);
-        unlink($temp_name);
-
-        return array(
-            "code" => utf8_decode(implode("\n", $output)),
-            "styles" => "styles/" . $style . ".css"
-        );
-
+    $params = " ";
+    foreach ($pygments_bind_params as $k => $v) {
+      $params .= $k . "=" . $v . " ";
     }
+
+    $command = $pygments_bind_app . " " . rtrim($params);
+    $output = array();
+    $retval = -1;
+
+    exec($command, $output, $retval);
+    unlink($temp_name);
+
+    return array(
+      "code" => utf8_decode(implode("\n", $output)),
+      "styles" => "styles/" . $style . ".css"
+    );
+
+  }
 
 
 }
